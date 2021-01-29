f7497fe7b3fe1b6cc38b845a16d9f47331ea2bd1
duchuynh0899 <duchuynh0899@gmail.com>
2021-01-29 22:41:46 +0700

    up

--- pages/index.vue
+++ pages/index.vue
@@ -8,21 +8,7 @@
       <h2 class="subtitle">
         My astonishing Nuxt.js project
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <p>Alo toi nghe day</p>
       </div>
     </div>
   </div>
